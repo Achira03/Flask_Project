ea4efecb9113381f9aa8ee7b56c371feee790ae2
Achira03 <achizx0@gmail.com>
2025-02-20 21:30:45 +0700

    make class User in models

---
+++ Flask_project/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)  # ใช้ String(256) เพื่อรองรับ hashed password
+    role = db.Column(db.String(20), default='user')
